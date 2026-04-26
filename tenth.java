3f771e828c3a9f80cb42a729f3081f73e5b4c311
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-26 13:50:23 +0530

    Add files via upload

---
+++ tenth.java
@@ -0,0 +1,11 @@
+class tenth {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(a << 1);
+        System.out.println(a >> 1);
+    }
+}
+
