d97597800704fcac3c6da2ebe94d56e8fa2b533b
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-26 13:48:47 +0530

    Add files via upload

---
+++ seventh.java
@@ -0,0 +1,10 @@
+public class seventh {
+    public static void main(String args []){
+        String name = "Surbhi";
+        int regNo = 190;
+        String branch="CSIT";
+        System.out.println(name);
+        System.out.println(regNo);
+        System.out.println(branch);
+    }
+}
