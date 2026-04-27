1018101260c21f021436e72dfbe5cc2b9d38a9b9
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-27 13:18:20 +0530

    Add files via upload

---
+++ ques5.java
@@ -0,0 +1,23 @@
+    import java.util.Scanner;
+    public class ques5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of levels: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
